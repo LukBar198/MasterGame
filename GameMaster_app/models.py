1fb832c58644692f647a8052b87ad2bb205e84ce
LukBar198 <lukasz.baran191@gmail.com>
2023-08-30 19:27:43 +0200

    Added User model in models.py.

--- GameMaster_app/models.py
+++ GameMaster_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    nickname = models.CharField(max_length=128, unique=True)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=64)
+    is_game_master = models.BooleanField(default=False)
+    is_player = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
