40fc7f9832d359cfb04ab655a6d8a33920116c63
LukBar198 <lukasz.baran191@gmail.com>
2023-09-01 17:14:15 +0200

    Added GameMaster and Player models in models.py.

--- GameMaster_app/models.py
+++ GameMaster_app/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    nickname = models.CharField(max_length=128, unique=True)
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=64)
+class GameMaster(models.Model):
+    nickname = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     is_game_master = models.BooleanField(default=False)
+    creation_date = models.DateTimeField(default=timezone.now)
+
+
+class Player(models.Model):
+    nickname = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     is_player = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
+    creation_date = models.DateTimeField(default=timezone.now)
