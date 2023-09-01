fbaa07ad986b385aaa5c1404d1a0660afb8988b9
LukBar198 <lukasz.baran191@gmail.com>
2023-09-01 18:03:00 +0200

    Added GameSession model, updated GameMaster and Player models in models.py.

--- GameMaster_app/models.py
+++ GameMaster_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class GameMaster(models.Model):
@@ -8,8 +9,41 @@ class GameMaster(models.Model):
     is_game_master = models.BooleanField(default=False)
     creation_date = models.DateTimeField(default=timezone.now)
 
+    class Meta:
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return self.nickname
+
 
 class Player(models.Model):
     nickname = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     is_player = models.BooleanField(default=True)
     creation_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return self.nickname
+
+
+class GameSession(models.Model):
+    class GameSystem(models.TextChoices):
+        RPG1 = 'Rpg1', 'Fajny system'
+        RPG2 = 'Rpg2', 'Dobry system'
+        RPG3 = 'Rpg3', 'Taki sobie system'
+        NONAME = 'NN', 'Inny system'
+
+    owner = models.ForeignKey(GameMaster, on_delete=models.CASCADE)
+    system = models.CharField(max_length=4, choices=GameSystem.choices, default=GameSystem.NONAME)
+    creation_date = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=256)
+    slots = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(6)])
+    session_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return self.title
