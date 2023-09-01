9f18cb80a770598f0bd689bb9c97e3c10f2009e1
LukBar198 <lukasz.baran191@gmail.com>
2023-09-01 21:14:35 +0200

    Added CharacterSheet model in models.py.

--- GameMaster_app/models.py
+++ GameMaster_app/models.py
@@ -6,6 +6,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class GameMaster(models.Model):
     nickname = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user_nickname = models.CharField(max_length=128, default='')
     is_game_master = models.BooleanField(default=False)
     creation_date = models.DateTimeField(default=timezone.now)
 
@@ -13,11 +14,12 @@ class GameMaster(models.Model):
         ordering = ['-creation_date']
 
     def __str__(self):
-        return self.nickname
+        return self.user_nickname
 
 
 class Player(models.Model):
-    nickname = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    player_nickname = models.CharField(max_length=128, default='')
     is_player = models.BooleanField(default=True)
     creation_date = models.DateTimeField(default=timezone.now)
 
@@ -25,7 +27,7 @@ class Player(models.Model):
         ordering = ['-creation_date']
 
     def __str__(self):
-        return self.nickname
+        return self.player_nickname
 
 
 class GameSession(models.Model):
@@ -35,7 +37,7 @@ class GameSession(models.Model):
         RPG3 = 'Rpg3', 'Taki sobie system'
         NONAME = 'NN', 'Inny system'
 
-    owner = models.ForeignKey(GameMaster, on_delete=models.CASCADE)
+    owner_id = models.ForeignKey(GameMaster, on_delete=models.CASCADE)
     system = models.CharField(max_length=4, choices=GameSystem.choices, default=GameSystem.NONAME)
     creation_date = models.DateTimeField(default=timezone.now)
     title = models.CharField(max_length=256)
@@ -53,12 +55,34 @@ class GameSession(models.Model):
 
 class PlayerCharacter(models.Model):
     class CharacterStatus(models.TextChoices):
-        DEAD = 'Dead', 'Żyje'
-        ALIVE = 'Alive', 'Martwy'
+        DEAD = 'Dead', 'Martwy'
+        ALIVE = 'Alive', 'Żyje'
 
-    owner = models.ForeignKey(Player, on_delete=models.CASCADE)
+    owner_id = models.ForeignKey(Player, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     description = models.TextField()
     creation_date = models.DateTimeField(default=timezone.now)
     character_status = models.CharField(max_length=5, choices=CharacterStatus.choices, default=CharacterStatus.ALIVE)
-    game_session = models.ForeignKey(GameSession, on_delete=models.CASCADE)
+    game_session_id = models.ForeignKey(GameSession, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return self.name
+
+
+class CharacterSheet(models.Model):
+    character_id = models.OneToOneField(PlayerCharacter, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=128)
+    creation_date = models.DateTimeField(default=timezone.now)
+    strength = models.PositiveIntegerField(default=8, validators=[MinValueValidator(1), MaxValueValidator(30)])
+    condition = models.PositiveIntegerField(default=8, validators=[MinValueValidator(1), MaxValueValidator(30)])
+    dexterity = models.PositiveIntegerField(default=8, validators=[MinValueValidator(1), MaxValueValidator(30)])
+    intelligence = models.PositiveIntegerField(default=8, validators=[MinValueValidator(1), MaxValueValidator(30)])
+    wisdom = models.PositiveIntegerField(default=8, validators=[MinValueValidator(1), MaxValueValidator(30)])
+    charisma = models.PositiveIntegerField(default=8, validators=[MinValueValidator(1), MaxValueValidator(30)])
+    reputation = models.IntegerField(default=0)
+    wealth = models.PositiveIntegerField(default=0)
+    life_points = models.PositiveIntegerField(default=10, validators=[MaxValueValidator(100)])
+    age = models.PositiveIntegerField(default=20, validators=[MinValueValidator(18), MaxValueValidator(999)])
