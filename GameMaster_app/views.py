d36b5f20c3919c20eea69f3ca49bbf8c5aa7b347
LukBar198 <lukasz.baran191@gmail.com>
2023-09-08 20:38:34 +0200

    Added User authentication

--- GameMaster_app/views.py
+++ GameMaster_app/views.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.contrib import messages
+from .forms import LoginForm
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        login_form = AuthenticationForm()
+        return render(request, 'index.html', {'login_form': login_form})
+
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            cd = login_form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Logowanie OK')
+                else:
+                    return HttpResponse('Konto zablokowane')
+            else:
+                messages.error(request, 'Nieprawidłowy login lub hasło.')
+        else:
+            login_form = LoginForm()
+        return render(request, 'index.html', {'login_form': login_form})
+
+# class SessionList(View):
+#     def get(self, request):
+#         ctx = {
+#             'SESSIONS': GameSession
+#         }
+#         return render(request, 'index.html', ctx)
--- MasterGame/settings.py
+++ MasterGame/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MasterGame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'GameMaster_app/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
